a8684507ab4c9912b35913f17ad5df4fd288b434
Manab-pokharel <manabpokh04@gmail.com>
2025-11-17 08:12:55 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+document.getElementById("homeBtn").onclick = () => {
+    window.location.href = "Home.html";
+};
+document.getElementById("aboutBtn").onclick = () => {
+    window.location.href = "about.html";
+};
+document.getElementById("contactBtn").onclick = () => {
+    window.location.href = "contact.html";
+};
+
---
+++ script2.js
@@ -0,0 +1,11 @@
+document.getElementById("openInsta").addEventListener("click", function () {
+    window.open("https://www.instagram.com/manabdgaf/", "_blank");
+});
+
+document.getElementById("openFb").addEventListener("click", function () {
+    window.open("https://www.facebook.com/manab.pokharel.2025", "_blank");
+});
+
+document.getElementById("openLinkedin").addEventListener("click", function () {
+    window.open("https://www.linkedin.com/in/manab-pokharel-92355a335/", "_blank");
+});
